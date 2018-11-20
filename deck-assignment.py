424f3f1c22885e364795313c690f9cbdbf62dec0
死の金属と数学。 <35822767+satanmyninjas@users.noreply.github.com>
2018-11-19 22:05:17 -0500

    Add required code + docstring, w/ reference
    Can't guarantee it will work. At the time of typing this, I'm about to pass out to go to sleep.

--- deck-assignment.py
+++ deck-assignment.py
@@ -1,22 +1,24 @@
-# Import statements go here
-
+import random
 
 class Deck:
     """
-    Your docstring goes here
+    This is used to represent a standard deck of cards.
+    
+    Use the .deal method to get a singular card as an output.
     """
 
     ranks = [str(r) for r in range(2, 11)] + list('JQKA')
     suits = list('cdhs')
-
+    # [do_stuff(i,j) for i in list1 for j in list2] (ex. of nested for loop in listcomp for reference)
     def __init__(self):
-        self._cards = []  # Your code goes inside the listcomp
+        self._cards = [range(2, 11) for element_1 in ranks for element_2 in suits]
 
     def __repr__(self):
-        pass
+        return f' Deck({card})'
 
     def __len__(self):
         return len(self._cards)
 
     def deal(self):
-        pass
+        return self._cards[random.randint(0, len(self._cards))]
+
